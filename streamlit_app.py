fbc0374f1f53b66ffcb693e185e262557246db6d
polresi <polituspol@gmail.com>
2025-05-12 22:27:53 +0200

    added requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from datetime import datetime
 import base64
-from streamlit_option_menu import option_menu
 import streamlit.components.v1 as components
 from PIL import Image
 
